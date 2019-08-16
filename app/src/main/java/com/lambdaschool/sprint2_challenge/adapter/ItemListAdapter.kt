2b10ea399e46b3e7feb9b7b7921efa6842bfa7b2
Deran Decker <me@derandecker.com>
2019-08-16 07:17:15 -1000

    Make switch active. Add notification.

--- app/src/main/java/com/lambdaschool/sprint2_challenge/adapter/ItemListAdapter.kt
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/adapter/ItemListAdapter.kt
@@ -8,22 +8,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Switch
 import android.widget.TextView
 import com.lambdaschool.sprint2_challenge.R
+import com.lambdaschool.sprint2_challenge.model.ShoppingItem
+import com.lambdaschool.sprint2_challenge.ui.MainActivity
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.recycler_cardview_layout.view.*
 
 class ItemListAdapter(val imageList: IntArray, val itemNameList: Array<String>)
     : RecyclerView.Adapter<ItemListAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val image: ImageView = view.iv_shopping_item
         val itemName: TextView = view.tv_shopping_item
+        val toggle: Switch = view.switch_toggle_item
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val viewGroup = LayoutInflater.from(parent.context).
-                inflate(R.layout.recycler_cardview_layout, parent, false)
+        val viewGroup = LayoutInflater.from(parent.context).inflate(R.layout.recycler_cardview_layout, parent, false)
         return ViewHolder(viewGroup)
     }
 
@@ -35,9 +38,16 @@ class ItemListAdapter(val imageList: IntArray, val itemNameList: Array<String>)
         val image = getDrawable(holder.image.context, imageList[position])
         holder.image.setImageDrawable(image)
         holder.itemName.text = itemNameList[position]
+        holder.toggle.setOnClickListener {
+            when (holder.toggle.isChecked) {
+                true -> MainActivity.shoppingList.add(itemNameList[position])
+                false -> MainActivity.shoppingList.remove(itemNameList[position])
+            }
+
+        }
+
+
     }
-
-
 
 
 }
--- app/src/main/java/com/lambdaschool/sprint2_challenge/ui/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/sprint2_challenge/ui/MainActivity.kt
@@ -1,29 +1,67 @@
 package com.lambdaschool.sprint2_challenge.ui
 
-import android.support.v7.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.app.NotificationCompat
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import com.lambdaschool.sprint2_challenge.R
 import com.lambdaschool.sprint2_challenge.ShoppingItemConstants
 import com.lambdaschool.sprint2_challenge.adapter.ItemListAdapter
-import com.lambdaschool.sprint2_challenge.model.ShoppingItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        var shoppingList = mutableListOf<String>()
+        const val NOTIFICATION_ID = 99
+        const val INTENT_KEY = "INTENT_KEY"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var shoppingList = mutableListOf<ShoppingItem>()
-
 
         rv_layout.setHasFixedSize(true)
         val manager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         val adapter = ItemListAdapter(ShoppingItemConstants.ICON_IDS, ShoppingItemConstants.ITEM_NAMES_RAW)
         rv_layout.layoutManager = manager
         rv_layout.adapter = adapter
+
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channelId = "$packageName.deranchannel"
+
+        btn_send_list.setOnClickListener {
+            // send notification saying Order has been placed
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val name = "Shopping List Notification"
+                val importance = NotificationManager.IMPORTANCE_HIGH
+                val description = "Notification showing verification that shopping list has been shared."
+
+                val channel = NotificationChannel(channelId, name, importance)
+                channel.description = description
+            }
+
+            val notificationBuilder = NotificationCompat.Builder(this, channelId)
+                    .setPriority(NotificationManager.IMPORTANCE_LOW)
+                    .setContentTitle("Shopping List")
+                    .setContentText("Your shopping list has been shared!")
+                    .setSmallIcon(android.R.drawable.ic_dialog_info)
+                    .setColor(Color.BLUE)
+
+            notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+
+            Log.d("SHOPPINGLIST", shoppingList.toString())
+
+            // send implicit intent with list of items
+        }
 
     }
 }
